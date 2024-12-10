c47255f98a0079c95f69a70849b22d4a4cb62fd9
Jenisha <jenisha@192.168.1.4>
2024-12-10 09:01:10 +0545

    4: handling request memoization

--- app/messages/layout.js
+++ app/messages/layout.js
@@ -1,9 +1,9 @@
 export default async function MessagesLayout({ children }) {
-  const response = await fetch("http://localhost:8080/messages", {
-    headers: {
-      "X-ID": "layout",
-    },
-  });
+  // here same request is in two places
+  // but when refreshing the page only one api gets called
+  // because nextJs caches
+  // this is called request memoization
+  const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
   const totalMessages = messages.length;
 
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,11 @@
 import Messages from "@/components/messages";
 
 export default async function MessagesPage() {
-  const response = await fetch("http://localhost:8080/messages", {
-    headers: {
-      "X-ID": "page",
-    },
-  });
+  // here same request is in two places
+  // but when refreshing the page only one api gets called
+  // because nextJs caches
+  // this is called request memoization
+  const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
