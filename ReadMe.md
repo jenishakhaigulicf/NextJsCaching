c97cda411dadac7fbf416dd694ca78ca2a268f5c
Jenisha <jenisha@192.168.1.4>
2024-12-10 09:23:55 +0545

    8: revalidatePath and revalidateTag

--- ReadMe.md
+++ ReadMe.md
@@ -4,20 +4,23 @@ Next14 aggressive caching > Next15
 
 1. Request memoization
    NextJs stores/memorizes the request with same configuration
+   at build and local time
    to Avoid duplicate data fetches
    persists until request duration
 
 2. Data Cache
    Stores and reuses data that has been fetched from the data source
+   at build and local time
    Avoids data fetches in whole unlike request memoization that avoids duplicate data fetches
    persists until revalidated
 
 3. Full Route Cache
    Not only the data, Next also caches the entire HTML and client side rendering
    at build time
-   Avoids re-rendering
+   Avoids re-rendering does pre-rendering
    persists until revalidated
 
 4. Router Cache
    managed on the client side
+
 # NextJsCaching
--- app/messages/page.js
+++ app/messages/page.js
@@ -5,12 +5,15 @@ import { unstable_noStore } from "next/cache";
 // export is needed
 // revalidate is a reserved word
 // export const revalidate = 5;
+// -------
+
 // export const dynamic = "force-dynamic"; // same as no-store
 
 export default async function MessagesPage() {
   // this is more recommended
   // than this export const dynamic = "force-dynamic";
   // unstable_noStore();
+  // -------
 
   // here same request is in two places
   // but when refreshing the page only one api gets called
@@ -18,10 +21,17 @@ export default async function MessagesPage() {
   // this is called request memoization
   const response = await fetch("http://localhost:8080/messages", {
     // cache: "no-store", //Next15
+    // -------
     // cache: "force-cache", // Next14
+    // -------
     // next: {
     //   revalidate: 5, // 5secs to wait for revalidation
     // },
+    // -------
+    // tag can be useful when revalidating different data fetch at the same time
+    // same tag can be used for the different fetches
+    // and revalidateTag("tagName") can be done to revalidate all
+    // next: { tags: ["msg"] },
   });
   const messages = await response.json();
 
